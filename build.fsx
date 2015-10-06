92f403d2f6a2bbde2edb3667c6885092e6409746
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-10-06 11:40:25 +0200

    Updates for WebSharper 3.5 + use F# 3.0

--- build.fsx
+++ build.fsx
@@ -4,31 +4,25 @@ open IntelliFactory.Build
 let bt =
     BuildTool().PackageId("WebSharper.Formlets.JQueryUI")
         .VersionFrom("WebSharper")
+        .WithFSharpVersion(FSharpVersion.FSharp30)
         .WithFramework(fun fw -> fw.Net40)
         .References(fun r ->
             [
-                r.NuGet("WebSharper.JQueryUI").Reference()
                 r.Assembly "System.Web"
+                r.NuGet("WebSharper.Html").ForceFoundVersion().Reference()
+                r.NuGet("WebSharper.JQueryUI").ForceFoundVersion().Reference()
+                r.NuGet("IntelliFactory.Reactive").ForceFoundVersion().Reference()
+                r.NuGet("WebSharper.Formlets").ForceFoundVersion().Reference()
             ])
 
 let main =
     bt.WebSharper.Library("WebSharper.Formlets.JQueryUI")
         .SourcesFromProject()
-        .References(fun r ->
-            [
-                r.NuGet("IntelliFactory.Reactive").Reference()
-                r.NuGet("WebSharper.Formlets").Reference()
-            ])
 
 let test =
     bt.WebSharper.HtmlWebsite("WebSharper.Formlets.JQueryUI.Tests")
         .SourcesFromProject()
-        .References(fun r ->
-            [
-                r.NuGet("IntelliFactory.Reactive").Reference()
-                r.NuGet("WebSharper.Formlets").Reference()
-                r.Project main
-            ])
+        .References(fun r -> [r.Project main])
 
 bt.Solution [
     main
